170b7ad7b009f23effe784d9647f2639740db1c2
Raj Ghosh <89258247+Rajghosh786@users.noreply.github.com>
2024-08-16 03:05:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
 const searchbtn = document.getElementById("searchbtn");
 const searchbar = document.getElementById("searchbar");
 const ull = document.getElementById("ullist");
-// const checkin = document.querySelector(".checkin")
+
 const checkout = document.querySelector(".checkout");
 const entrylist = document.querySelector(".entrylist");
 const unorderd = document.querySelector(".unorderd");
 
-// let todo = document.querySelector(".todo")
 
-
-// checkin.addEventListener('click', function(){
-//     todo.classList.toggle("line")
-// })
 
 searchbtn.addEventListener('click',append);
 searchbtn.addEventListener('click',clear);
-// entrylist.addEventListener('click',filtertodo);
+
 
     function clear(){
         searchbar.value = "";
     }
     function append(event){
         if(searchbar.value.length > 0){
-            event.preventDefault();
-            // event.preventDefault makes it not disappear in refresh
-
             const creatediv = document.createElement('div');
                 creatediv.classList.add("firstlist"); 
             const createli = document.createElement('li');
@@ -67,43 +59,3 @@ searchbtn.addEventListener('click',clear);
         ull.innerHTML = localStorage.getItem("data");
     }
     showdata();
-    // function deletedata(){
-    //     ull.innerHTML = localStorage.removeItem("data");
-    // }
-// function filterlist(e){
-//     const todos = ull.childNodes[3];
-//     console.log(todos);
-//     // console.log(e.target.value);
-//     // todos.map(function(todo){
-//     //     if(e.target.value === "all"){
-//     //        todo.style.display = "hide"; 
-//     //     }else{
-//     //         console.log('error')
-//     //     }
-//     // })
-// }
-
-// function filtertodo(e){
-//     const todos = ull.childNodes;
-//     todos.forEach(function(todo){
-//         switch(e.target.value){
-//             case "all":
-//                 todo.style.display = "flex";
-//             break;
-//             case "completed":
-//                 if(todo.classList.contains("completed")){
-//                     todo.style.display = "flex";
-//                 }else{
-//                     todo.style.display = "none";
-//                 }
-//             break;
-//             case "completed":
-//                 if(!todo.classList.contains("completed")){
-//                     todo.style.display = "flex";
-//                 }else{
-//                     todo.style.display = "none";
-//                 }
-//             break;
-//         }   
-//     })
-// }
