d058a4f875fe1888847431a96283b594a763f450
Raj Ghosh <89258247+Rajghosh786@users.noreply.github.com>
2024-08-16 16:56:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const searchbtn = document.getElementById("searchbtn");
 const searchbar = document.getElementById("searchbar");
 const ull = document.getElementById("ullist");
-
 const checkout = document.querySelector(".checkout");
 const entrylist = document.querySelector(".entrylist");
 const unorderd = document.querySelector(".unorderd");
-
-
+const all = document.querySelector(".all");
+const completed = document.querySelector(".completed");
+const pending = document.querySelector(".pending");
+const firstlist = document.querySelector(".firstlist")
 
 searchbtn.addEventListener('click',append);
 searchbtn.addEventListener('click',clear);
@@ -17,6 +18,9 @@ searchbtn.addEventListener('click',clear);
     }
     function append(event){
         if(searchbar.value.length > 0){
+            event.preventDefault();
+
+
             const creatediv = document.createElement('div');
                 creatediv.classList.add("firstlist"); 
             const createli = document.createElement('li');
@@ -38,7 +42,6 @@ searchbtn.addEventListener('click',clear);
 
     ull.addEventListener('click',deleting)
     function deleting(e){
-        // console.log(e.target.parentElement)
         const item = e.target
         if(item.classList[0] === "checkout"){         
         const removeelement = item.parentElement;
@@ -52,6 +55,24 @@ searchbtn.addEventListener('click',clear);
         };
     }
 
+    entrylist.addEventListener('click',function(data){
+        console.log(data.target)
+        const todos = ull.childNodes
+        console.log(todos)
+        todos.forEach(element => {
+            if(data.target.value === "pending" && element.classList.contains('line')){
+                console.log(element)
+                element.classList.toggle("none")
+            }
+            if(data.target.value === "completed" && !element.classList.contains('line')){
+                console.log(element)
+                element.classList.toggle("none")
+            }
+            savedata()
+        });
+    
+    })
+
     function savedata(){
         localStorage.setItem("data",ull.innerHTML);
     }
@@ -59,3 +80,7 @@ searchbtn.addEventListener('click',clear);
         ull.innerHTML = localStorage.getItem("data");
     }
     showdata();
+    // function deletedata(){
+    //     ull.innerHTML = localStorage.removeItem("data");
+    // }
+    // deletedata()
